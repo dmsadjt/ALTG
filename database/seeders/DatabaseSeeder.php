7408a756ca0646c04c007b0559b08604764c0d67
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-06-22 20:31:39 +0700

    minor update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(PositionSeeder::class);
+        $this->call(RaritySeeder::class);
         $this->call(ShipsSeeder::class);
 
 
